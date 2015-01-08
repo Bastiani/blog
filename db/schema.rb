aa72debd7b79b9047bf5e37694673b44d9f12b2e
Bastiani <rafacdb@gmail.com>
2015-01-08 17:08:54 -0200

    Novo layout e paginação

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150106235851) do
+ActiveRecord::Schema.define(version: 20150108160143) do
 
   create_table "posts", force: true do |t|
     t.string   "titulo"
     t.text     "texto"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
